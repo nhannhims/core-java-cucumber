e8d7d63727d9c9143591f0790a755baea7919217
Vuong Huu Nhan <nhims@MacBook-Pro-cua-Vuong.local>
2023-12-14 15:47:21 +0700

    init project automation with cucumber

---
+++ src/main/java/com/nhims/main/App.java
@@ -0,0 +1,13 @@
+package com.nhims.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
